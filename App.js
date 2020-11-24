38b1188f45eef3a25bf1633a23da79a9062e5e7a
Fernando Vinicius Conceição Araujo <45467298801@info.local>
2020-11-24 16:51:31 -0300

    Preparação de ambiente finalizada

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+
+//Pages
+//import Home from './pages/Home'
+//import Login from './pages/Login';
+
+//Libs
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+//Instanciando Libs
+const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
+const Stack  = createStackNavigator();
+
 
 export default function App() {
+  //Colocar Navigation
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Projeto Iniciado</Text>
     </View>
   );
 }
@@ -14,7 +31,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#9200D6',
     alignItems: 'center',
     justifyContent: 'center',
   },
